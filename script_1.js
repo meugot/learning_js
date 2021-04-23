3cf256e0f4af3820fe971ba15e6772fa7cb473ea
Abhishek <meugot190@gmail.com>
2021-04-23 23:04:04 +0530

    window.alert()

--- script_1.js
+++ script_1.js
@@ -1,10 +1,14 @@
 function onClickButton(){
-    document.getElementById("js-inject").innerHTML = window.prompt("What do you want to enter");
+    document.getElementById("js-inject").innerHTML = "Showed after clicking button";
 }
 
 function tryDocWrite(){
     // Using document.write() after an HTML page is loaded will remove all the existing HTML.
     document.write(4+8);
+}
+
+function alertUser(){
+    window.alert("Alerting User!!")
 }
 
 function consoleLog(){
